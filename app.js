2e1610d0ea16c5399ab8ff43565c2d7bae81c612
Max Cramer <maximiliancramer@yahoo.com>
2019-09-25 15:05:00 +0100

    Built two different for loops, practicing

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ for (var i = 0; i < 7; i++) {
     console.log(string = string.concat('#')); // redefining string in the console.log as loops over and changes every time
   }
 }
+
+var string1 = '';
+for (var i = 0; i < 7; i++) {
+  string1 += 'O'; // += means + string 
+  console.log(string1);
+}
